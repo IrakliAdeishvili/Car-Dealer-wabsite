893109b61bf5101f1b264ca140fce841085ba18d
IrakliAdeishvili <146751884+IrakliAdeishvili@users.noreply.github.com>
2025-04-03 17:36:59 +0400

    update html

--- car-dealer/src/componnets/CarCatalog.jsx
+++ car-dealer/src/componnets/CarCatalog.jsx
@@ -1,6 +1,11 @@
 import './carCatalog.scss';
 import arrowIcon from "../assets/arrow-up-right.svg";
 import toyotaImg from "../assets/carsCard/toyota.png";
+import teslaImg from "../assets/carsCard/tesla.png";
+import audiImg from "../assets/carsCard/audi.png";
+import forrdImg from "../assets/carsCard/forrd.png";
+import hondaImg from "../assets/carsCard/honda.png";
+import kiaImg from "../assets/carsCard/kia.png";
 import dashboardIcon from "../assets/carsCard/dashboard.svg";
 import petrolIcon from "../assets/carsCard/petrol.svg";
 import gearshiftIcon from "../assets/carsCard/gearshift.svg";
@@ -40,9 +45,9 @@ function CarCatalog(){
                 </div>
 
                 <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
+                    <img className='card-img' src={forrdImg} alt="" />
                     <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
+                       <h2 class="card-title">2024 Ford Explorer</h2>
                       <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
                     </div>
                     <div className="cardIcon">
@@ -54,17 +59,17 @@ function CarCatalog(){
                       
                     </div>
                     <div className="card-footer">
-                    <p>60,000$</p>
+                    <p>80,000$</p>
                     <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
                     </div>
 
                 </div>
         
                 <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
+                    <img className='card-img' src={hondaImg} alt="" />
                     <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
+                       <h2 class="card-title">2025 Honda Pilot  </h2>
+                      <p class="card-text">2.0L  4 Cylinder A-FORCE MAX Hybrid</p><hr />
                     </div>
                     <div className="cardIcon">
                         <div className="cardIcon-content">
@@ -75,22 +80,22 @@ function CarCatalog(){
                       
                     </div>
                     <div className="card-footer">
-                    <p>60,000$</p>
+                    <p>40,000$</p>
                     <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
                     </div>
 
                 </div>
         
                 <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
+                    <img className='card-img' src={kiaImg} alt="" />
                     <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
+                       <h2 class="card-title">2025 Kia Telluride </h2>
+                      <p class="card-text">2.0L 4 Cylinder  MAX Hybrid</p><hr />
                     </div>
                     <div className="cardIcon">
                         <div className="cardIcon-content">
                         <p className='cardIcon-text'><img  src={dashboardIcon} alt="" />50 Miles</p>
-                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Petrol</p>
+                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Hybrid </p>
                         <p className='cardIcon-text'><img  src={gearshiftIcon} alt="" />Automatic</p>
                         </div><hr />
                       
@@ -102,10 +107,10 @@ function CarCatalog(){
 
                 </div>
                 <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
+                    <img className='card-img' src={audiImg} alt="" />
                     <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
+                       <h2 class="card-title">2025 Audi Q7</h2>
+                      <p class="card-text">3.5L 6  Cylinder i-FORCE MAX Hybrid </p><hr />
                     </div>
                     <div className="cardIcon">
                         <div className="cardIcon-content">
@@ -116,67 +121,27 @@ function CarCatalog(){
                       
                     </div>
                     <div className="card-footer">
-                    <p>60,000$</p>
+                    <p>100,000$</p>
                     <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
                     </div>
 
                 </div>
                 <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
+                    <img className='card-img' src={teslaImg} alt="" />
                     <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
+                       <h2 class="card-title">Tesla Model X</h2>
+                      <p class="card-text">Full Elecro  4 Cylinder MAX Hybrid</p><hr />
                     </div>
                     <div className="cardIcon">
                         <div className="cardIcon-content">
                         <p className='cardIcon-text'><img  src={dashboardIcon} alt="" />50 Miles</p>
-                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Petrol</p>
+                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Elecro</p>
                         <p className='cardIcon-text'><img  src={gearshiftIcon} alt="" />Automatic</p>
                         </div><hr />
                       
                     </div>
                     <div className="card-footer">
-                    <p>60,000$</p>
-                    <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
-                    </div>
-
-                </div>
-                <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
-                    <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
-                    </div>
-                    <div className="cardIcon">
-                        <div className="cardIcon-content">
-                        <p className='cardIcon-text'><img  src={dashboardIcon} alt="" />50 Miles</p>
-                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Petrol</p>
-                        <p className='cardIcon-text'><img  src={gearshiftIcon} alt="" />Automatic</p>
-                        </div><hr />
-                      
-                    </div>
-                    <div className="card-footer">
-                    <p>60,000$</p>
-                    <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
-                    </div>
-
-                </div>
-                <div className="car-card">
-                    <img className='card-img' src={toyotaImg} alt="" />
-                    <div class="card-content">
-                       <h2 class="card-title">Toyota Land Cruiser</h2>
-                      <p class="card-text">2.4L 4 Cylinder i-FORCE MAX Hybrid</p><hr />
-                    </div>
-                    <div className="cardIcon">
-                        <div className="cardIcon-content">
-                        <p className='cardIcon-text'><img  src={dashboardIcon} alt="" />50 Miles</p>
-                        <p className='cardIcon-text'><img  src={petrolIcon} alt="" />Petrol</p>
-                        <p className='cardIcon-text'><img  src={gearshiftIcon} alt="" />Automatic</p>
-                        </div><hr />
-                      
-                    </div>
-                    <div className="card-footer">
-                    <p>60,000$</p>
+                    <p>70,000$</p>
                     <li><a href="">View All <img src={arrowIcon} alt="" /></a></li>
                     </div>
 
